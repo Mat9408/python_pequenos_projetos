e48b4df1c9b7f6531c13de679fd2dc243c68c3ca
Matheus Alves <99809494+Mat9408@users.noreply.github.com>
2022-07-12 09:56:47 -0300

    Delete soma100numerosnaturais.py

--- soma100numerosnaturais.py
+++
@@ -1,6 +0,0 @@
-#Construir um programa que apresente a soma dos cem primeiros números naturais.
-
-soma = 0
-for contador in range(0,101):
-    soma += contador
-print(f"A soma dos 100 primeiros números naturais é: {soma}.")
