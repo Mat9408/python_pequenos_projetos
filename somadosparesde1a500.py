d07821580624391695ba477af4447851d57df728
Matheus Alves <99809494+Mat9408@users.noreply.github.com>
2022-07-12 09:57:24 -0300

    Delete somadosparesde1a500.py

--- somadosparesde1a500.py
+++
@@ -1,8 +0,0 @@
-# Calcular a soma dos números pares de 1 a 500.
-
-numero = 0
-soma = 0
-while(numero <=500):
-    soma = soma + numero 
-    numero += 2
-    print(f"A soma dos números pares de 1 a 500 é: {soma}.")
